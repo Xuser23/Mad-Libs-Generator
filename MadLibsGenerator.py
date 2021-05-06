63d9e53939dc0e9bbc8524c131c3491d56eb8854
Xuser23 <devamahendrar@gmail.com>
2021-05-06 14:20:37 +0700

    First Mad Libs

---
+++ MadLibsGenerator.py
@@ -0,0 +1,12 @@
+def MadLib1():
+    animals = input("Enter a Animal: ")
+    profession = input("Enter a Profession: ")
+    cloth = input("Enter a Cloth: ")
+    things = input("Enter a Things: ")
+    name = input("Enter a Name: ")
+    place = input("Enter a Place: ")
+    verb = input("Enter a Verb: ")
+    food = input("Enter a Food: ")
+    print('say ' + food + ', the photographer said as the camera flashed! ' + name + ' and I had gone to ' + place +' to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending to be a ' + profession + '. when we saw the second photo, it was exactly what I wanted. We both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind')
+
+MadLib1()
